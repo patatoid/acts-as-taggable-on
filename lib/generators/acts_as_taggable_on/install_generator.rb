5260b13d79f181485a023c4f63430121cc0fffe8
Pascal Knoth <pascal@knoth.fr>
2015-06-19 17:38:52 +0200

    generator for migrations

---
+++ lib/generators/acts_as_taggable_on/install_generator.rb
@@ -0,0 +1,13 @@
+module ActsAsTaggableOn
+	module Generators
+		class InstallGenerator < Rails::Generators::Base
+			desc "create necessary migrations"
+		  def migrations
+				i = 0
+				%w{acts_as_taggable_on_migration.rb add_missing_unique_indices.rb add_taggings_counter_cache_to_tags.rb add_missing_taggable_index.rb change_collation_for_tag_names.rb}.each do |migration|
+        	template migration, "db/migrate/#{(Time.now + (i++).second).to_s(:number)}_#{migration}"
+				end
+		  end
+		end
+	end
+end
